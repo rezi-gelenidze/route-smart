e247ccd87aab19bccaca615d85d697578144344e
rezi-gelenidze <rezi.gelenidze7@gmail.com>
2024-05-26 21:12:48 +0400

    add creation serializers

---
+++ src/main/java/hopla/routesmart/dto/CreateTripDTO.java
@@ -0,0 +1,40 @@
+package hopla.routesmart.dto;
+
+public class CreateTripDTO {
+    private Long identifier;
+    private Long fromId;
+    private Long toId;
+
+    // Constructors, getters, and setters
+    public CreateTripDTO() {}
+
+    public CreateTripDTO(Long identifier, Long fromId, Long toId) {
+        this.identifier = identifier;
+        this.fromId = fromId;
+        this.toId = toId;
+    }
+
+    public Long getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(Long identifier) {
+        this.identifier = identifier;
+    }
+
+    public Long getFromId() {
+        return fromId;
+    }
+
+    public void setFromId(Long fromId) {
+        this.fromId = fromId;
+    }
+
+    public Long getToId() {
+        return toId;
+    }
+
+    public void setToId(Long toId) {
+        this.toId = toId;
+    }
+}
