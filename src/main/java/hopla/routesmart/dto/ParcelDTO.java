dfc8925936b382b788733893caba892a5badf115
rezi-gelenidze <rezi.gelenidze7@gmail.com>
2024-05-26 18:39:22 +0400

    define dto for serialization

---
+++ src/main/java/hopla/routesmart/dto/ParcelDTO.java
@@ -0,0 +1,9 @@
+package hopla.routesmart.dto;
+
+public interface ParcelDTO {
+    Long getParcelId();
+    Long getFromId();
+    String getFromDisplay();
+    Long getToId();
+    String getToDisplay();
+}
---
+++ src/main/java/hopla/routesmart/dto/TripDTO.java
@@ -0,0 +1,9 @@
+package hopla.routesmart.dto;
+
+public interface TripDTO {
+    Long getTripId();
+    Long getFromId();
+    String getFromDisplay();
+    Long getToId();
+    String getToDisplay();
+}
