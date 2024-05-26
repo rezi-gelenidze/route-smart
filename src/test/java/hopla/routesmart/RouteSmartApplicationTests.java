a1f7475812646cd53ec4578c7f1bbf718ac9f0fc
rezi-gelenidze <rezi.gelenidze7@gmail.com>
2024-05-26 15:48:26 +0400

    add blank tests

---
+++ src/test/java/hopla/routesmart/RouteSmartApplicationTests.java
@@ -0,0 +1,13 @@
+package hopla.routesmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouteSmartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
