62f42dcc731035c5c2baede29b36bf4a28ffd7cb
rezi-gelenidze <rezi.gelenidze7@gmail.com>
2025-04-22 02:50:03 +0400

    clear main runner

--- src/main/java/hopla/routesmart/RouteSmartApplication.java
+++ src/main/java/hopla/routesmart/RouteSmartApplication.java
@@ -1,19 +1,12 @@
 package hopla.routesmart;
 
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class RouteSmartApplication implements CommandLineRunner {
-
-
+public class RouteSmartApplication {
     public static void main(String[] args) {
         SpringApplication.run(RouteSmartApplication.class, args);
     }
-
-    @Override
-    public void run(String... args) throws Exception {}
 }
